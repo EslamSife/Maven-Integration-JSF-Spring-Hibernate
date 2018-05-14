f8d59d4b5b18aacc0c56387215ad4a7e1d7518a0
DESKTOP-H8LN1KQ\EslamSife <O-Eslam.SifeElDin@Etisalat.Com>
2018-05-14 10:22:06 +0200

    add missing imports for Pattern  regx class

--- src/com/ldevs/springjsf/service/AddNewServiceFilesImpl.java
+++ src/com/ldevs/springjsf/service/AddNewServiceFilesImpl.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.regex.Pattern;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
